bf21cf58a7075621c6c2e410a3a2ed3dffe40b7c
lefirea <vocaloidlove04@gmail.com>
2021-11-04 23:33:12 +0900

    Update ImageMaskingClass.py

--- ImageMaskingClass.py
+++ ImageMaskingClass.py
@@ -4,8 +4,9 @@ import numpy as np
 
 class ImageMaskingClass:
     def __init__(self):
-        self.upper = np.array([179, 255, 255])
-        self.lower = np.array([0, 0, 0])
+        # self.upper = np.array([179, 255, 255])
+        # self.lower = np.array([0, 0, 0])
+        pass
 
     def filterMasking(self, img, hsv):
         self.mask = cv2.inRange(hsv, self.lower, self.upper)
@@ -18,42 +19,42 @@ class StopSign(ImageMaskingClass):
     def __init__(self):
         super(StopSign).__init__()
         self.upper = np.array([30, 255, 255])
-        self.lower = np.array([20, 100, 100])
+        self.lower = np.array([20, 50, 50])
 
 
 class Speed10Sign(ImageMaskingClass):
     def __init__(self):
         super(Speed10Sign).__init__()
         self.upper = np.array([5, 255, 255])
-        self.lower = np.array([0, 100, 100])
+        self.lower = np.array([0, 50, 50])
 
 
 class Speed50Sign(ImageMaskingClass):
     def __init__(self):
         super(Speed50Sign).__init__()
         self.upper = np.array([115, 255, 255])
-        self.lower = np.array([105, 100, 100])
+        self.lower = np.array([105, 50, 50])
 
 
 class Speed100Sign(ImageMaskingClass):
     def __init__(self):
         super(Speed100Sign).__init__()
         self.upper = np.array([50, 255, 255])
-        self.lower = np.array([40, 100, 100])
+        self.lower = np.array([40, 50, 50])
 
 
 class RightSign(ImageMaskingClass):
     def __init__(self):
         super(RightSign).__init__()
         self.upper = np.array([85, 255, 255])
-        self.lower = np.array([75, 100, 100])
+        self.lower = np.array([75, 50, 50])
 
 
 class LeftSign(ImageMaskingClass):
     def __init__(self):
         super(LeftSign).__init__()
         self.upper = np.array([140, 255, 255])
-        self.lower = np.array([130, 100, 100])
+        self.lower = np.array([120, 50, 50])
 
 
 class ArrowSign(ImageMaskingClass):
@@ -61,7 +62,7 @@ class ArrowSign(ImageMaskingClass):
         super(ArrowSign).__init__()
         # 右向きと同じ色（向き検出が可能なので色分けする必要がない）
         self.upper = np.array([85, 255, 255])
-        self.lower = np.array([75, 100, 100])
+        self.lower = np.array([75, 50, 50])
 
     def getArrowDirection(self):
         # 矢印の外接矩形を検出
